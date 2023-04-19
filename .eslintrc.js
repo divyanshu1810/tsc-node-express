9d8fe75856c5bf27bd2b0e478c50322ee6b8ad0d
Divyanshu Yadav <dd8353@srmist.edu.in>
2023-04-19 18:11:03 +0530

    :tada: ts-repo Initialised

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.export = {
+    parser: "@typescript-eslint/parser",
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended",
+    ],
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    rules: {},
+};
